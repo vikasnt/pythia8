4b2c83f0a71ac481c560dc661008d4c95e873310
vikas <vikasgupta.rmps@gmail.com>
2017-06-03 16:16:53 +0530

    first attmept at working pythia6 plugin

--- examples/main36.cc
+++ examples/main36.cc
@@ -2,7 +2,13 @@
 #include "Pythia8Plugins/LHAPythia6.h"
 using namespace Pythia8;
 int main() {
-  Pythia pythia;
-  LHAupPythia6 pythia6;
+  Pythia py8;
+  LHAupPythia6 py6(&py8,"CMS", "p", "p", 14000); 
+  py8.readString("Beams:frameType = 5");
+  py8.init();
+  for (int iEvent = 0; iEvent < 100; ++iEvent) 
+  py8.next();
+  // End of run.
+  py8.stat();
   return 0;
 }
